8302903c34f8aad8dfc138816a649d2171f4bd34
Davis JR <davisjr2000@hotmail.com>
2019-05-12 12:06:44 -0300

    Fetch Pokemon From API

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  state = {
+    pokemons: []
+  }
+
+  componentDidMount(){
+    this.loadPokemons();
+  }
+
+  loadPokemons = async () => {
+    const api = axios.create({
+      baseURL: 'http://localhost:3000/api/v1'
+    })
+    const response = await api.get('/pokemons')
+
+    const pokemons = response.data;
+
+
+    this.setState({ pokemons: pokemons });
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Pokémons!</Text>
+        {this.state.pokemons.map(pokemon => {
+           return <Text>{pokemon.name}</Text>
+        })}
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
